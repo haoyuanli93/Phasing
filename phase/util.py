73dbd7a417fa5afe1766a1568146985b745f8397
Haoyuan Li <hyli16@stanford.edu>
2019-01-12 20:49:38 -0800

    minor

---
+++ phase/util.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy as sp
+
+"""
+    Some functions are shared by both cpu and gpu algorithms,
+    I'll just put them here.
+    
+"""
+
+def shrink_wrap():
+    pass
